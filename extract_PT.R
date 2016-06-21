e0cdeca29b0c55f09b9b8b5f97ec888d473472a9
kah5 <kellyannheilman@gmail.com>
2016-06-20 21:08:49 -0400

    added plots for precip and climate & discretized distributions

--- extract_PT.R
+++ extract_PT.R
@@ -33,7 +33,13 @@ y$total <- rowSums(y[,c('Jan', 'Feb', 'Mar', "Apr", "May",
 
 #this averages for each month within each gridcell
 full <- dcast(setDT(y), Lon + Lat ~ ., value.var=c('Jan', 'Feb', 'Mar', "Apr", "May", 
-                                               'Jun', "Jul", "Aug", "Sep", "Oct", "Nov","Dec", 'total'))
+                                               'Jun', "Jul", "Aug", "Sep", "Oct", "Nov","Dec",'total'))
+library(plyr)
+library(reshape2)
+
+min.pr <- dcast(data.frame(y), Lon + Lat ~.,min , value.var= 'total')
+max.pr <-  dcast(data.frame(y), Lon + Lat ~.,max , value.var= 'total')
+
 
 #convert to rasterstack
 coordinates(full) <- ~Lon + Lat
@@ -128,7 +134,7 @@ for (i in years) {
   y$year <- i
 }
 
-y$annual <- rowMeans(y[,c('Jan', 'Feb', 'Mar', "Apr", "May", 
+y$annual <- rowSums(y[,c('Jan', 'Feb', 'Mar', "Apr", "May", 
                         'Jun', "Jul", "Aug", "Sep", "Oct", "Nov","Dec")])
 
 #this averages for each month within each gridcell
@@ -175,7 +181,7 @@ for (i in years) {
   y$year <- i
 }
 
-y$annual <- rowMeans(y[,c('Jan', 'Feb', 'Mar', "Apr", "May", 
+y$annual <- rowSums(y[,c('Jan', 'Feb', 'Mar', "Apr", "May", 
                         'Jun', "Jul", "Aug", "Sep", "Oct", "Nov","Dec")])
 
 #this averages for each month within each gridcell
@@ -370,7 +376,7 @@ hist(tree.dens$., breaks = 25, xlab = "density (stems/ha)", main = 'Histogram of
 hist(basal$., breaks = 50, xlab = 'basal area (m^2/m^2)', main = 'Histogram of Basal Area')
 
 #plot(avg.prism.p$., basal$.)
-plot(precip$total_., basal$., ylim = c(0, 100), col=ifelse(basal$.>=40,"red","black"), ylab = 'Basal area (m^2/m^2)', xlab = 'Prism 1900 precipitation')
+plot(precip$total_., basal$., ylim = c(0, 100), col=ifelse(basal$.>=40,"red","black"), ylab = 'Basal area (m^2/m^2)', xlab = 'GHCN 1900-1910 precipitation')
 rect(1200,-10, 1000,300, col = rgb(0.5,0.5,0.5,1/4))
 rect(1200, -10, 725, 300, col = rgb(red = 1, 0, 0, alpha = 1/4))
 rect(1200,-10, 1000,300, col = rgb(0.75,0.75,0.75,1/2)) # add the range of tropical savanna intermediate climate
@@ -380,6 +386,32 @@ legend("topleft",
        legend = c("Midwest intermediate climate", "Tropical intermediate", '>40 m^/m2 BA', '<40% m^2/m^2 BA'), 
        col = c(rgb(red = 1, 0, 0, alpha = 1/4), rgb(0.75,0.75,0.75,1/2), 'red', 'black'), 
        pch = c(15,15,1,1))
+
+plot(air_temp$range_., tree.dens$.,col=dens.discrete$bins, ylab = 'Tree density (stems/ha)', xlab = 'GHCN mean annual Temp range')
+
+plot(air_temp$annual_., tree.dens$.,col=dens.discrete$bins, ylab = 'Tree density (stems/ha)', xlab = 'GHCN mean annual Temp')
+
+plot(PET$total_., dens.discrete$.,col= dens.discrete$bins, ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Jul_., tree.dens$.,col=dens.discrete$bins, ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Jun_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$May_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Apr_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Aug_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Sep_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(PET$Oct_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual P-ET')
+
+plot(Eo150$annual_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual Potential ET')
+
+plot(E150$annual_., tree.dens$.,col=ifelse(tree.dens$.>=53,"red","black"), ylab = 'Tree density (stems/ha)', xlab = 'GHCN annual ET')
+
+
 dev.off()
 
 
@@ -409,7 +441,7 @@ plot(tropics$Annual.temperature.range/10, tropics$Effective.Rainfall,
      ylab = 'Effective rainfall (mm)', xlab = 'Annual temperature range (DegC)')
 points(air_temp$range_., PET$total_., col = 'red')
 legend('bottomleft', pch =1, col = c('black', 'red'), c('Tropics', 'midwest'))
-
+points(air_temp$range_., PET$total_., col = 'red')
 # the mean annual precipitation is not listed in the dataset
 hist(tropics$Tree.Basal.Area..m2.ha.)
 hist(basal$., breaks = 50)
@@ -431,32 +463,74 @@ mapdata<-data.frame(mapdata)
 
 dens.map <- ggplot()+ geom_raster(data=tree.dens, aes(x=x, y=y, fill = .))+
   labs(x="easting", y="northing", title="Tree raw density") + 
-  scale_fill_gradientn(colours = rainbow(4), name ="Tree Density (stems/ha)")
+  scale_fill_gradientn(colours = rainbow(4), name ="Tree Dens. \n (stems/ha)")
 #dens.map <- sites.map + geom_point(data = priority, aes(x = coords.x1, y = coords.x2, shape = Description))+geom_text_repel(data = priority,aes(x = coords.x1, y = coords.x2,label=code))
 dens.map <- dens.map +geom_polygon(data=data.frame(mapdata), aes(x=long, y=lat, group=group),colour = "Black", fill = NA)
 dens.map
 
+
+dens.sd.map <- ggplot()+ geom_raster(data=tree.dens.sd, aes(x=x, y=y, fill = .))+
+  labs(x="easting", y="northing", title="Tree density standard deviation") + 
+  scale_fill_gradientn(colours = rainbow(3), name ="Tree Dens. sd ")
+#dens.map <- sites.map + geom_point(data = priority, aes(x = coords.x1, y = coords.x2, shape = Description))+geom_text_repel(data = priority,aes(x = coords.x1, y = coords.x2,label=code))
+dens.sd.map <- dens.sd.map +geom_polygon(data=data.frame(mapdata), aes(x=long, y=lat, group=group),colour = "Black", fill = NA)
+dens.sd.map
+
+sd.points <- ggplot(data=dens.discrete, aes(x=tree.dens$., y = tree.dens.sd$., color= type)) + geom_point()+
+  xlab('Tree density (stems/ha)')+ylab('Standard deviation of tree density')
+
 dens.discrete$type <- dens.discrete$bins
 dens.discrete[dens.discrete$type == 1, ]$type <- '>85 stems/ha'
-dens.discrete[dens.discrete$type == 2, ]$type <- '<50 stems/ha'
-dens.discrete[dens.discrete$type == 3, ]$type <- '50-85 stems/ha'
+dens.discrete[dens.discrete$type == 2, ]$type <- '<53 stems/ha'
+dens.discrete[dens.discrete$type == 3, ]$type <- '53-85 stems/ha'
 
 dens.dis.map <- ggplot()+ geom_raster(data=dens.discrete, aes(x=x, y=y, fill =type))+
-  labs(x="easting", y="northing", title="Tree density binned") #+ 
+  labs(x="easting", y="northing", title="Tree dens. \n binned") #+ 
  # scale_fill_gradientn(colours = rainbow(4), name ="Tree Density (stems/ha)")
 #dens.map <- sites.map + geom_point(data = priority, aes(x = coords.x1, y = coords.x2, shape = Description))+geom_text_repel(data = priority,aes(x = coords.x1, y = coords.x2,label=code))
 dens.dis.map <- dens.dis.map +geom_polygon(data=data.frame(mapdata), aes(x=long, y=lat, group=group),colour = "Black", fill = NA)
 dens.dis.map
 
+#put all these into pdf document
+pdf('C:/Users/JMac/Documents/Kelly/biomodality/outputs/density.maps.pdf')
+dens.hist <- ggplot(data=dens.discrete, aes(x=., fill=type) )+
+  geom_histogram(binwidth=5, alpha=.5, position="identity") + xlab('Tree density (stems/ha)') 
 
-dens.hist <- ggplot(data=dens.discrete, aes(x=., fill=bins) )+
-  geom_histogram(binwidth=5, alpha=.5, position="identity") 
+ggplot(data=dens.discrete, aes(x=precip$total_., y = ., color=type)) + geom_point() +
+  xlab('Mean annual precip. (mm)') + ylab('Density (stems/ha)')
+
+ggplot(data=dens.discrete, aes(x=air_temp$annual_., y = ., color=type)) + geom_point() +
+  xlab('Annual Air Temp (DegC)') + ylab('Density (stems/ha)')
+
+ggplot(data=dens.discrete, aes(x=PET$total_., y = ., color=type)) + geom_point() +
+  xlab('P-ET (mm)') + ylab('Density (stems/ha)')
+
+ggplot(data=dens.discrete, aes(x= E150$annual_., y = ., color=type)) + geom_point() +
+  xlab('Mean annual PET') + ylab('Density (stems/ha)')
+
+ggplot(data=dens.discrete, aes(x=Eo150$annual_., y = ., color=type)) + geom_point() +
+  xlab('Mean annual ET)') + ylab('Density (stems/ha)')
+
+ggplot(data=air_temp, aes(x=precip$total_., y = air_temp$annual_., color=dens.discrete$type)) + geom_point() +
+  xlab('Mean annual precip. (mm)') + ylab('Annual air temp. (DegC)')
+
+ggplot(data=air_temp, aes(x=PET$total_., y = air_temp$range_., color=dens.discrete$type)) + geom_point() +
+  xlab('Effective rainfall. (mm)') + ylab('Annual Air temperature range (DegC)')
+
+plot(tropics$Annual.temperature.range/10, tropics$Effective.Rainfall, 
+     ylab = 'Effective rainfall (mm)', xlab = 'Annual temperature range (DegC)')
+points(air_temp$range_., PET$total_., col = 'red')
+legend('bottomleft', pch =1, col = c('black', 'red'), c('Tropics', 'midwest'))
+points(air_temp$range_., PET$total_., col = 'red')
 
 
-pdf('outputs/density.maps.pdf')
-
+sd.points
 dens.map
 dens.dis.map
 dens.hist
-
+dens.sd.map
 dev.off()
+
+
+# based on this, I expect tree cover to be bimodal above 725 mm/year (1900-1910), with GHCN data, and 850 with 1895-1905 prism data
+# the low frequency intermediate of density occurs from 50-85 stems/ha

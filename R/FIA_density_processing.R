ee732cb3d0e3496fc08d93f4b2a7de7d4513e24f
kah5 <kellyannheilman@gmail.com>
2016-08-24 17:11:15 -0400

    Added calculation of species richness and shannon diversity for pls and fia datasets

--- R/FIA_density_processing.R
+++ R/FIA_density_processing.R
@@ -37,6 +37,7 @@ colnames(umdw.new) <- c('x', 'y', 'cell', 'PLSdensity')
 
 pls.inil <- rbind(pls.inil, umdw.new)
 #write.csv(pls.inil,C:/Users/JMac/Documents/Kelly/biomodality/outputs )
+
 #merge inil pls and inilFIA
 densitys <- merge(pls.inil[,c('x', 'y', 'cell', 'PLSdensity')], density.FIA.table[,c('x', 'y', 'cell', 'FIAdensity')],
                   by = c('x', 'y', 'cell'))
@@ -61,8 +62,15 @@ to.add.umdw <-pls.names[!pls.names %in% umdw.names]
 to.add.pls <- umdw.names[!umdw.names %in% pls.names]
 
 #add these species columns to the respective dataframes, but with 0 for data values
-pls.spec[,to.add.pls] <- 0
-umdw[,to.add.umdw] <- 0
+pls.spec[,to.add.pls] <- NA
+umdw[,to.add.umdw] <- NA
+
+
+#now I need to add columns to FIA dataframe for species not seen in FIA, but seen in PLS:
+
+
+
+
 
 #reorder the columns so the pls.spec and umdw dataframes match
 pls.spec<- pls.spec[ , order(names(pls.spec))]
@@ -88,6 +96,69 @@ full.spec<- full.spec %>%
   select(-total, everything())
 
 #now add totals to the 'total columns
+full.spec$total <- rowSums(full.spec[,5:41], na.rm = TRUE)
+summary(full.spec$total)
+hist(full.spec$total, breaks = 1000, xlim = c(0,600))
+
+colnames(full.spec)[42] <- 'PLSdensity'
+
+#now I need to add columns to FIA dataframe for species not seen in FIA, but seen in PLS:
+
+pls.full.n<- colnames(full.spec)
+fia.names<- colnames(density.FIA.table)
+fia.names[!fia.names %in% pls.full.n]
+pls.full.n[!pls.full.n %in% fia.names]
+
+colnames(density.FIA.table)[11] <- 'Cedar.juniper'
+colnames(density.FIA.table)[19] <- 'Other.hardwood'
+fia.names[!fia.names %in% pls.full.n]
+pls.full.n[!pls.full.n %in% fia.names]
+
+density.FIA.table<- density.FIA.table[,-4] # remove Var.4
+density.FIA.table<- density.FIA.table[,-5]# remove atlantic white cedar
+density.FIA.table<- density.FIA.table[,-23] #remove total
+
+pls.names<- colnames(full.spec)
+fia.names<- colnames(density.FIA.table)
+
+#create name vectors for species columns missing in the upper and lower midewst
+to.add.fia <-pls.names[!pls.names %in% fia.names]
+#to.add.pls <- fia.names[!fia.names %in% pls.names]
+
+#add these species columns to the respective dataframes, but with 0 for data values
+#pls.spec[,to.add.pls] <- NA
+density.FIA.table[,to.add.fia] <- NA
+density.FIA.table<- density.FIA.table[,-42] #remove plsdensity columns
+
+FIA.full <- density.FIA.table[,order(names(density.FIA.table))]
+
+FIA.full<- FIA.full %>%
+  select(cell, everything())
+
+FIA.full<- FIA.full %>%
+  select(y, everything())
+
+FIA.full<- FIA.full %>%
+  select(x, everything())
+
+FIA.full<- FIA.full %>%
+  select(X, everything())
+
+#FIA.full<- FIA.full %>%
+ # select(-total, everything())
+
+#now add totals to the 'total columns
+FIA.full$total <- rowSums(FIA.full[,5:41], na.rm = TRUE)
+summary(FIA.full$total)
+hist(FIA.full$total, breaks = 50, xlim = c(0,600))
+
+
+
+
+################################################################
+#comparison of FIA and PLS datasets to climate
+###############################################################
+
 #past.precip <- read.csv('data/pr_alb_1895_1935_GHCN.csv')
 #mod.precip <- read.csv('data/pr_alb_1975_2014_GHCN.csv')
 
@@ -95,24 +166,36 @@ past.precip <- read.csv('outputs/pr_monthly_Prism_1895_1905.csv')
 mod.precip <- read.csv('data/spec_table_30yr_prism.csv')
 
 
-dens.pr <- merge(densitys, past.precip[,c('x', 'y', 'total_.')], by =c('x', 'y'))
-
+dens.pr <- merge(full.spec, past.precip[,c('x', 'y', 'total_.')], by =c('x', 'y'))
 dens.pr <- merge(dens.pr, mod.precip[,c('x', 'y', 'pr30yr')], by = c('x', 'y'))
-colnames(dens.pr) <- c('x', 'y', 'cell', 'PLSdensity', 'FIAdensity', 'MAP1910', "MAP2011")
+colnames(dens.pr)[42:44] <- c('PLSdensity', 'MAP1910', "MAP2011")
 
+fia.dens.pr <- merge(FIA.full, past.precip[,c('x', 'y', 'total_.')], by =c('x', 'y'))
+fia.dens.pr <- merge(fia.dens.pr, mod.precip[,c('x', 'y', 'pr30yr')], by = c('x', 'y'))
+colnames(fia.dens.pr)[42:44] <- c('FIAdensity', 'MAP1910', "MAP2011")
 
-write.csv(dens.pr, "C:/Users/JMac/Documents/Kelly/biomodality/data/midwest_pls_fia_density_pr_alb.csv")
+write.csv(dens.pr, "C:/Users/JMac/Documents/Kelly/biomodality/data/midwest_pls_density_pr_alb.csv")
+write.csv(fia.dens.pr, "C:/Users/JMac/Documents/Kelly/biomodality/data/midwest_FIA_density_pr_alb.csv")
 
-dens.pr[dens.pr$PLSdensity>1000,]$PLSdensity <- 700
+dens.pr[dens.pr$PLSdensity>1000,]$PLSdensity <- 1000 #patch fix the overestimates of density
+
 #plot histograms
-hist(dens.pr$PLSdensity, breaks = 50)
-hist(dens.pr$FIAdensity, breaks = 50)
+hist(dens.pr$PLSdensity, breaks = 50, xlim = c(0,1000), xlab = 'PLS density (stems/ha)', main = 'PLS Midwest Density')
+hist(fia.dens.pr$FIAdensity, breaks = 50, xlim = c(0,1000), xlab = 'FIA density(stems/ha)',main = 'FIA Midwest Density')
 
 #plot raw data
 plot(dens.pr$MAP1910,dens.pr$PLSdensity, xlab = 'Past MAP', ylab = 'PLS density')
-plot(dens.pr$MAP2011,dens.pr$FIAdensity, xlab = 'Modern MAP', ylab = 'Modern density')
+plot(fia.dens.pr$MAP2011,fia.dens.pr$FIAdensity, xlab = 'Modern MAP', ylab = 'Modern density')
 plot(dens.pr$MAP2011, dens.pr$PLSdensity, xlab = 'Modern MAP', ylab = 'PLS density')
-plot(dens.pr$MAP1910, dens.pr$FIAdensity, xlab = 'Past MAP', ylab = 'Modern density')
+plot(fia.dens.pr$MAP1910, fia.dens.pr$FIAdensity, xlab = 'Past MAP', ylab = 'Modern density')
+
+fia.dens.pr[is.na(fia.dens.pr)] <- 0
+
+dens.pr <- merge(dens.pr, fia.dens.pr[,c('x', 'y', 'cell', 'FIAdensity')], by = c('x', 'y', 'cell'))
+
+
+
+summary(rowSums(dens.pr != 0, na.rm=TRUE))
 
 PLS.lm<- lm(dens.pr$PLSdensity ~dens.pr$MAP1910)
 FIA.lm<- lm(dens.pr$FIAdensity ~dens.pr$MAP2011)
@@ -126,19 +209,58 @@ summary(PLS_mod.lm)
 summary(FIA_pas.lm)
 summary(diff.lm)
 
+
 dens.pr$diff <- dens.pr$FIAdensity - dens.pr$PLSdensity
 plot(dens.pr$PLSdensity, dens.pr$diff, xlab='PLS tree density (trees/ha)', ylab='increase in density since PLS (trees/ha)')
+abline(a = 0, b = 0, col = 'red')
+
+##calculate species richness: number of species in each grid cell
+dens.pr$spec.rich <- rowSums(dens.pr[,5:41] != 0, na.rm=TRUE)
+fia.dens.pr$spec.rich <- rowSums(fia.dens.pr[,5:41] != 0, na.rm=TRUE)
+
+#species richness is way lower in FIA compared to PLS
+
+#calculate frequency of species i---pi = # trees of species i/total number of trees
+#H = - SUM(pi *ln(pi))
+pi.pls <- dens.pr[,5:41]/dens.pr$PLSdensity
+
+pi.fia <- fia.dens.pr[,5:41]/fia.dens.pr$FIAdensity
+
+dens.pr$H.pls <- -1*rowSums((pi.pls)*log(pi.pls), na.rm=TRUE)
+fia.dens.pr$H.fia <- -1*rowSums((pi.fia)*log(pi.fia), na.rm=TRUE)
+
+#calculate simpsons index (lambda) = sum (#trees species*(# trees of species i-1))/(total density*(total density -1))
+#simpsons gives probability that any two random tress will be different species
+#this gives some negative probabilities so double check
+lambda.pls <- rowSums((dens.pr[,5:41]*(dens.pr[,5:41]-1))/(dens.pr$PLSdensity*(dens.pr$PLSdensity-1)), na.rm = TRUE)
+lambda.fia <- rowSums((fia.dens.pr[,5:41]*(fia.dens.pr[,5:41]-1))/(fia.dens.pr$PLSdensity*(fia.dens.pr$FIAdensity-1)), na.rm = TRUE)
+
+#similarity index to compare pls & FIA
+#sim = 2*sum(n*c)
+
 
 #dens.pr<- data.frame(dens.pr)
 library(ggplot2)
-pls.map <- ggplot()+ geom_raster(data=dens.pr, aes(x=x, y=y, fill = PLSdensity))+
+melt.test <- melt(dens.pr[,1:41], id = c('x', 'y', 'cell', 'X'))
+pls.2map <- ggplot()+ geom_raster(data=melt.test, aes(x=x, y=y, fill = value))+
   labs(x="easting", y="northing", title="Tree PLS density") + 
-  scale_fill_gradientn(colours = rainbow(4), name ="Tree Dens. \n (stems/ha)")
+  scale_fill_gradientn(colours = rev(terrain.colors(4)), name ="Tree Dens. \n (stems/ha)") + facet_wrap(~variable)
+pls.2map
+
+X11(width = 18)
 pls.map
 
-FIA.map <- ggplot()+ geom_raster(data=dens.pr, aes(x=x, y=y, fill = FIAdensity))+
+sc <- scale_colour_gradientn(colours = rev(terrain.colors(8)), limits=c(0, 16))
+
+
+pls.map <- ggplot()+ geom_raster(data=dens.pr, aes(x=x, y=y, fill = spec.rich))+
+  labs(x="easting", y="northing", title="Tree PLS density") + 
+  scale_fill_gradientn(colours = rev(terrain.colors(4)), limits = c(0,16), name ="Species \n Richness (S)") 
+pls.map
+
+FIA.map <- ggplot()+ geom_raster(data=fia.dens.pr, aes(x=x, y=y, fill = spec.rich))+
   labs(x="easting", y="northing", title="Tree FIA density") + 
-  scale_fill_gradientn(colours = rainbow(4), name ="Tree Dens. \n (stems/ha)")
+  scale_fill_gradientn(colours = rev(terrain.colors(4)), limits = c(0,16), name ="Species \n Richness (S)") 
 FIA.map
 
 diff.map <- ggplot()+ geom_raster(data=dens.pr, aes(x=x, y=y, fill = diff))+
@@ -173,6 +295,10 @@ dif.pr
 dif <- ggplot()+ geom_point(data=dens.pr, aes(x=PLSdensity, y=FIAdensity))+
   geom_smooth(data=dens.pr, aes(x=PLSdensity, y=FIAdensity),method=lm) 
 dif
+
+
+
+
 
 ##################################################################################
 #find grid cells that have FIA data in them, but no PLS data in indiana & illinois

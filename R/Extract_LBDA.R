c060353046fcce3119b8f8ccf9f6f002506e71c0
kah5 <kellyannheilman@gmail.com>
2017-01-24 16:32:00 -0500

    Initial commit for extracting the LBDA data

---
+++ R/Extract_LBDA.R
@@ -0,0 +1,24 @@
+# investigating the LBDA
+library(ncdf4)
+library(raster)
+library(sp)
+library(ggplot2)
+
+LBDA <- brick('C:/Users/Jmac/Box Sync/LBDA/nada_hd2_cl.nc')
+LBDA <- nc_open('C:/Users/Jmac/Box Sync/LBDA/nada_hd2_cl.nc')
+summary(LBDA$var)
+LBDA.out <- list()
+LBDA.out$lat <- ncvar_get(LBDA, "lat")
+LBDA.out$lon <- ncvar_get(LBDA, "lon")
+LBDA.out$Year <- ncvar_get(LBDA, "time")
+#LBDA.out$pdsi <- ncvar_get(LBDA, "pdsi")
+for(v in names(LBDA$var)){
+  LBDA.out[[v]] <- ncvar_get(LBDA, v)  
+}
+
+#test plot the LBDA
+test<- raster(LBDA.out$pdsi[1850,138:1,1:237])
+plot(test)
+              
+# lets make a raster brick of this
+LBDAbrick<- brick(LBDA.out$pdsi[1750:2006,138:1,1:237])

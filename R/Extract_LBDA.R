34ac8d6f2f90749ef2817dc2909a0a46bdae5524
kah5 <kellyannheilman@gmail.com>
2017-01-24 19:00:06 -0500

    created raster brick of PDSI data

--- R/Extract_LBDA.R
+++ R/Extract_LBDA.R
@@ -11,18 +11,28 @@ LBDA.out <- list()
 LBDA.out$lat <- ncvar_get(LBDA, "lat")
 LBDA.out$lon <- ncvar_get(LBDA, "lon")
 LBDA.out$Year <- ncvar_get(LBDA, "time")
-#LBDA.out$pdsi <- ncvar_get(LBDA, "pdsi")
+# LBDA.out$pdsi <- ncvar_get(LBDA, "pdsi")
 for(v in names(LBDA$var)){
   LBDA.out[[v]] <- ncvar_get(LBDA, v)  
 }
 
-PDSI <- LBDA.out$pdsi
+PDSI <- LBDA.out$pdsi[1700:2006,138:1,1:237]
 
-new.data <- aperm(PDSI, c(2,3,1))
+new.data <- aperm(PDSI, c(2,3,1)) # need to reorder array so it has x,y,pdsi
 
-#test plot the LBDA
-test<- raster(LBDA.out$pdsi[1850,138:1,1:237])
-plot(test)
-              
 # lets make a raster brick of this
-LBDAbrick<- brick(LBDA.out$pdsi[, 138:1, 1:237])
+PDSI.brk<- brick(new.data)
+names(PDSI.brk) <- 1700:2006
+plot(PDSI.brk[[1]])
+plot(PDSI.brk$X1936)
+              
+#now lets average drought over 1700:1800 and 1900:2000
+
+spec.table <- read.csv('C:/Users/JMac/Documents/Kelly/biomodality/data/midwest_pls_full_density_pr_alb1.6-5.csv')
+coordinates(spec.table) <- ~x +y
+proj4string(spec.table) <- '+init=epsg:3175'
+spec.table.ll<- spTransform(spec.table, crs('+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0 '))
+
+t <- crop(s, extent(spec.table.ll)) 
+s <- projectRaster(t, crs='+init=epsg:3175') # project in great lakes albers
+y <- data.frame(rasterToPoints(s)) #covert to dataframe

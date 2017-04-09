de84c940f7b3287574a0c2935488c91e5902c35a
kah5 <kellyannheilman@gmail.com>
2017-04-08 22:48:12 -0400

    made some basic plots for the UMW

--- R/read_FOD.R
+++ R/read_FOD.R
@@ -30,25 +30,44 @@ hist(fires$FIRE_SIZE, xlim = c(0,1000),breaks = 1000000)
 summary(fires[fires$FIRE_SIZE >= 22902201,])
 length(fires[fires$FIRE_SIZE >= 22902201,])
 
-# calculate the proportion of each fire class in the region:
-A <- nrow(fires[fires$FIRE_SIZE_ %in% "A",])/nrow(fires)
-B <- nrow(fires[fires$FIRE_SIZE_ %in% "B",])/nrow(fires)
-C <- nrow(fires[fires$FIRE_SIZE_ %in% "C",])/nrow(fires)
-D <- nrow(fires[fires$FIRE_SIZE_ %in% "D",])/nrow(fires)
-E <- nrow(fires[fires$FIRE_SIZE_ %in% "E",])/nrow(fires)
-f <- nrow(fires[fires$FIRE_SIZE_ %in% "F",])/nrow(fires)
-G <- nrow(fires[fires$FIRE_SIZE_ %in% "G",])/nrow(fires)
 
-pct.fireclass <- data.frame(fireclass = c("A", "B", "C", "D", "E", "F", "G"), 
-                            pct = c(A, B, C, D, E, f, G), 
-                            classize= c("0 - 0.25", "0.26- 9.9", "10 - 99.9", "100 - 299", 
-                                        "300 - 999", "1000 - 4999", "5000+"))
+#looking at fire causes:
+library(plyr)
+fires$STAT_CAU_1 <- as.character(fires$STAT_CAU_1)
+bycause <- count(fires$STAT_CAU_1)
+bycause$freq <- bycause$freq/82205
 
-ggplot(pct.fireclass, aes(x= classize, y = pct))+geom_bar(stat = "identity")+theme_bw()+
-    ylab("Porportion of total Upper Midwest Fires 1992 - 2011") + xlab("Fire size class (acres)")
+png("outputs/proportion_byfirecause.png")
+ggplot(bycause, aes(x= x, y = freq))+geom_bar(stat = "identity")+theme_bw()+
+  ylab("Porportion of total Upper Midwest Fires \n 1992 - 2011") + xlab("Cause of fire")+ theme(axis.text.x = element_text(angle = 90, hjust = 1))
+dev.off()
+
+# plot the fire class 
+
+fires$FIRE_SIZE_ <- as.character(fires$FIRE_SIZE_)
+bysize <- count(fires$FIRE_SIZE_)
+bysize$freq <- bysize$freq/82205
+
+sizelookup <- data.frame(FIRE_SIZE_ = c("A", "B", "C", "D", "E", "F", "G"),
+                sizes = c("0 - 0.25", "0.26 - 9.9", "10.0-99.9", "100-299",
+                          "300 - 999", "1000 - 4999", "1000 - 4999"))
+
+bysize <- merge(bysize, sizelookup, by.x = "x",by.y = "FIRE_SIZE_")
+png("outputs/proportion_byfiresize.png")
+ggplot(bysize, aes(x= sizes, y = freq))+geom_bar(stat = "identity")+theme_bw()+
+  ylab("Proportion of total Upper Midwest Fires \n 1992 - 2011") + xlab("Size of Fire (acres)")
+dev.off()
 
 
-# these very large fires seem to be accurate:
+# plot the size by the fire class
+test <- count(df = fires, vars = c('FIRE_SIZE_', 'STAT_CAU_1'))
+test$pct <- test$freq/82205
+test <- merge(test, sizelookup, by= "FIRE_SIZE_")
+
+png("outputs/proportion_by_fire_size__and_cause.png")
+ggplot(test, aes(sizes, y = pct, fill = STAT_CAU_1))+geom_bar(stat="identity")+theme_bw()+
+  xlab("Fire Class") + ylab("Proportion of fires by size and cause 1992 - 2011") + guides(fill=guide_legend(title="Cause"))
+dev.off()
 
 # find average fire size per grid scale
 MW <- raster("data/upper_midwest.tif")

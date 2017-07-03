97470899ff9d2fd6eb03baead3f4f7b9931d2ecb
kah5 <kellyannheilman@gmail.com>
2017-07-02 20:37:11 -0500

    updating rolling bimodality plot

--- R/rolling_bimodality.R
+++ R/rolling_bimodality.R
@@ -12,14 +12,15 @@ pls.full <- full[full$period %in% "PLS",]
 
 rollBC_r = function(x, y, xout, width) { # x and y are the environment val and the density/comp that we want to determin bimodality with
   
-  out = numeric(length(xout))
+  out <- data.frame(xout = xout,
+                   out = NA)
   
   for( i in seq_along(xout) ){
     window = x >= (xout[i]-width) & x <= (xout[i]+width)
-    out[i] = bimodality_coefficient( na.omit(y[ window ] ) )# what is the BC for places with less than 300 trees per hectare
+    out[i,]$out = bimodality_coefficient( na.omit(y[ window ] ) )# what is the BC for places with less than 300 trees per hectare
   }
   
-  ggplot()+geom_point(aes(x = xout, y = out))+
+  ggplot(out)+geom_point(aes(x = xout, y = out))+
     geom_hline( yintercept = 5/9)+ylim(0,1)+theme_bw()+
     xlab('interval center') + ylab('Bimodality Coefficient') +ggtitle(paste0( 'Bimodality coefficient for binwidth = ', width))
   
@@ -35,4 +36,6 @@ rollBC_r(x = ordered$PC1, y = ordered$Density, xout = ordered$PC1, width = 1)
 ordered <- pls.full[order(pls.full$PC1),]
 ordered$rownum <- 1:length(ordered$PC1)
 
+rollBC_r(x = ordered$PC1, y = ordered$pc2, xout = ordered$PC1, width = 1)
 rollBC_r(x = ordered$PC1, y = ordered$pc2, xout = ordered$PC1, width = 0.5)
+rollBC_r(x = ordered$PC1, y = ordered$pc2, xout = ordered$PC1, width = 0.25)

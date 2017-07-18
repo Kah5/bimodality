834e6377429c88ff36f4759912c9e8955830bf0e
kah5 <kellyannheilman@gmail.com>
2017-07-18 14:03:07 -0500

    edited conceptual figure and added plotly density plots to bimodality draft

--- R/conceptual_figure.R
+++ R/conceptual_figure.R
@@ -1,28 +1,29 @@
 library(ggplot2) 
 
 # make dummy datasets of Modern and PLS:
-one <- data.frame(time = "FIA", value = rnorm(n = 4000, mean = -2, sd = 3))
-two <- data.frame(time = "PLS", value = rnorm(n = 4000, mean = 2, sd = 3))
+
+x3 = rnorm(2000, -3, 2)
+x4 = rnorm(2000,3,2)
+x3 <- c(x3,x4)
+one <- data.frame(time = "Modern", value = rnorm(n = 4000, mean = -2, sd = 2), climate = x3)
+two <- data.frame(time = "Past", value = rnorm(n = 4000, mean = 2, sd = 2), climate=x3)
 full <- rbind(one, two)
 
-label.breaks <- function(beg, end, splitby){
-  labels.test <- data.frame(first = seq(beg, end, by = splitby), second = seq((beg + splitby), (end + splitby), by = splitby))
-  labels.test <- paste (labels.test$first, '-' , labels.test$second)
-  labels.test
-}
 
 # use the label.breaks function and cut to cut environmental data up into different bins
 
-full$bins <-  cut(full$value, breaks = seq(-5, 6, by = 1), labels = label.breaks(-5, 5, 1))
+full$bins <-  cut(full$climate, breaks = seq(-16, 16, by = 2))
 
 
 
 png("outputs/conceptual_fig_mesophication.png")
 ggplot(full, aes(x = value, fill = time))+geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
-  scale_fill_manual(values = c("red", "blue"), limits = c("FIA", "PLS"))#+theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+  scale_fill_manual(values = c("red", "blue"), limits = c("Modern", "Past"))#+theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+dev.off()
 
-ggplot(full, aes(x = value, fill = time))+geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
-  scale_fill_manual(values = c("red", "blue"), limits = c("FIA", "PLS"))+facet_wrap(~bins, scales = "free_x")#theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+png(height = 3, width = 8, units = 'in', res = 300, "outputs/conceptual_fig_mesophication_panel.png")
+ggplot(full[!full$bins %in% c("(-10,-8]", '(-12,-10]','(8,10]','(10,12]'),], aes(x = value, fill = time))+geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
+  scale_fill_manual(values = c("red", "blue"), limits = c("Modern", "Past"))+facet_wrap(~bins, scales = "free_x", ncol = 5)#theme(axis.text.x = element_blank(), axis.title.x = element_blank())
 
 dev.off()
 
@@ -30,74 +31,56 @@ dev.off()
 
 #one <- data.frame(time = "PLS", value = rnorm(n = 1000, mean = 40, sd=20))
 #three <- data.frame(time = "PLS", value = rnorm(n = 1000, mean = 20, sd = 20))
-two <- data.frame(time = "FIA", value = rnorm(n = 4000, mean = 200, sd = 50))
+#two <- data.frame(time = "FIA", value = rnorm(n = 4000, mean = 200, sd = 50))
+
 
 n = 2000
-y1 = rnorm(n, 20, 40)  
-y2 = rnorm(n, 150, 50)
+y1 = rnorm(n, 25, 15)  
+y2 = rnorm(n, 100, 25)
 w = rbinom(n, 1, .5)                      # 50:50 random choice
 x2 = w*y1 + (1-w)*y2      
-x3 = rnorm(2000, 0, 4)
-one <- data.frame( time = "PLS", value = x2, climate = x3)
-two <- data.frame(time = "FIA", value = rnorm(n = 4000, mean = 200, sd = 50), climate = x3)
-
+x3 = rnorm(2000, -3, 2)
+x4 = rnorm(2000,3,2)
+x3 <- c(x3,x4)
+x2 <- x2[order(x2)]
+#x3 <- x3[order(x3)]
+one <- data.frame( time = "Past", value = x2, climate = x3)
+one[one$value <= 0,]$value <- 0
+one$climate<- ifelse(one$value <= 55, one$climate - 1, one$climate + 1)
+two <- data.frame( time = "Modern", value = rnorm(n = 4000, mean = 100, sd = 40 ), climate = x3)
+two[two$value <0,]$value <- 0
 
 full <- rbind(one, two)
-
-#gam()
-# need to somehow get climate data that allows for intermediate bimodality
 
 # use the label.breaks function and cut to cut environmental data up into different bins
 
 full$bins <-  cut(full$climate, breaks = seq(-16, 16, by = 2))
 
-ggplot(full, aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
-  scale_fill_manual(values = c("red", "blue"), limits = c("FIA", "PLS"))+facet_wrap(~bins, scales ="free_x")#theme(axis.text.x = element_blank(), axis.title.x = element_blank())
+ggplot(full, aes(x = climate, y = value))+geom_point()+facet_wrap(~time)
 
 
 
+ggplot(full[!full$bins %in% c("(-10,-8]", '(-12,-10]','(8,10]','(10,12]'),], aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
+  scale_fill_manual(values = c("red", "blue"), limits = c("Modern", "Past"))+xlab("Tree Density")
 
+
+
+# write outputs
 png("outputs/conceptual_fig_density.png")
-ggplot(full, aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
-  scale_fill_manual(values = c("red", "blue"), limits = c("FIA", "PLS"))+theme(axis.text.x = element_blank(), axis.title.x = element_blank())
-
-ggplot(full, aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
-  scale_fill_manual(values = c("red", "blue"), limits = c("FIA", "PLS"))+facet_wrap(~bins, scales ="free_x")#theme(axis.text.x = element_blank(), axis.title.x = element_blank())
-
+ggplot(full[!full$bins %in% c("(-10,-8]", '(-12,-10]','(8,10]','(10,12]'),], aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
+  scale_fill_manual(values = c("red", "blue"), limits = c("Modern", "Past"))+xlab("Tree Density")
 dev.off()
 
-mu1 <- log(1)   
-mu2 <- log(50)
-sig1 <- log(3)
-sig2 <- log(3)
-cpct <- 0.4   
-
-bimodalDistFunc <- function (n,cpct, mu1, mu2, sig1, sig2) {
-  y0 <- rnorm(n,mean=mu1, sd = sig1)
-  y1 <- rnorm(n,mean=mu2, sd = sig2)
-  
-  flag <- rbinom(n,size=1,prob=cpct)
-  y <- y0*(1 - flag) + y1*flag 
-}
-
-bimodalData <- bimodalDistFunc(n=1000,cpct,mu1,mu2, sig1,sig2)
-hist(log(bimodalData))
+png(height = 6, width = 8, units = 'in',res = 200,'outputs/conceptual_fig_density_by_bins.png')
+ggplot(full[!full$bins %in% c("(-10,-8]", '(-12,-10]','(8,10]','(10,12]'),], aes(x = value, fill = time)) + geom_histogram(alpha = 0.5, position = "identity")+theme_bw()+
+  scale_fill_manual(values = c("red", "blue"), limits = c("Modern", "Past"))+facet_wrap(~bins, scales ="free_x", ncol=4)+xlab("Tree Density")
+dev.off()
 
 
-n = 500;  
-#x1 = rbeta(n, .5, .4)
-y1 = rnorm(n, -4, 3)  
-y2 = rnorm(n, 4, 2)
-w = rbinom(n, 1, .5)                      # 50:50 random choice
-x2 = w*y1 + (1-w)*y2                      # normal mixture model of bimodality
+ggplot(full, aes(x = climate, y = value))+geom_point()+facet_wrap(~time)
 
+library(plotly)
 
+kd <- MASS::kde2d(one$value, one$climate, n = 25)
+p <- plot_ly(x = kd$x, y = kd$y, z = kd$z) %>% add_surface()
 
-par(mfrow=c(1,2))                         # two panels per plot
-hist(x1, prob=T, col="skyblue2", main="BETA(.5, .4)");  rug(x1)
-curve(dbeta(x, .5, .4), lwd=2, col="blue", add=T)
-
-
-hist(x2, prob=T, col="skyblue2", main="Normal Mixture"); 
-rug(x2)
-curve(.5*dnorm(x,5,2)+.5*dnorm(x,10,1), lwd=2, col="blue", add=T)

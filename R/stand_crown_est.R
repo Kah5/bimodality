cb8bf9ce7a406469f0552b7cd82850ec2a61d027
Kelly Heilman <Kah5@users.noreply.github.com>
2017-07-18 19:51:24 -0500

    Delete stand_crown_est.R

--- R/stand_crown_est.R
+++
@@ -1,7 +0,0 @@
-#Estimated Stand Crown cover from diameter data
-# using the equation from USFS
-
-#diameter data
-diams <- read.csv("data/plss_diam_alb_v0.9-10.csv")
-diams[,4:ncol(diams)]<- diams[,4:ncol(diams)]*2.54
-SCC = 0.0175 + 0.205*rowSums(diams[,4:ncol(diams)]) + 0.0060*rowSums(diams[,4:ncol(diams)]^2)

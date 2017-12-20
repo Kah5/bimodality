1ab48200a95fbf7202872d5c98d77aaefbc6595b
Kah5 <kellyannheilman@gmail.com>
2017-12-20 15:57:56 -0500

    changing random sampling strategy

--- R/NMDS_randcell_fia_pls.R
+++ R/NMDS_randcell_fia_pls.R
@@ -29,47 +29,47 @@ fullcomps <- fullcomps[!duplicated(fullcomps[,6:36]),]
 #df <- fullcomps[sample(nrow(fullcomps), 1000), ]
 df <- fullcomps
 
-randcellpls <- df[sample(x = nrow(df[df$period %in% "PLS",]) , size = 1500, replace = TRUE),]
-randcellfia <- df[sample(x = nrow(df[df$period %in% "FIA",]) , size = 1500, replace = TRUE),]
+randcellpls <- df[sample(x = nrow(df[df$period %in% "PLS",]) , size = 1500, replace = FALSE),]
+randcellfia <- df[sample(x = nrow(df[df$period %in% "FIA",]) , size = 1500, replace = FALSE),]
 randcell <- rbind(randcellpls, randcellfia)
-system.time(NMDS <- metaMDS(as.matrix(randcell[,6:36]), trymax = 50))
+system.time(NMDS <- metaMDS(as.matrix(randcell[,6:36]), trymax = 100))
 
 NMDS 
-saveRDS(NMDS, "data/NMDS_bimodal_only_fia_pls.rds")
+saveRDS(NMDS, "data/NMDS_bimodal_only_fia_pls2.rds")
 
-png('data/stressplot_rand_fia_pls.png')
+png('data/stressplot_rand_fia_pls2.png')
 stressplot(NMDS)
 dev.off()
 
-png('data/plot_NMDS_rand_fia_pls.png')
+png('data/plot_NMDS_rand_fia_pls2.png')
 plot(NMDS)
 dev.off()
 
-png('data/plot_NMDS_rand_spec_fia_pls.png')
+png('data/plot_NMDS_rand_spec_fia_pls2.png')
 MDSplot<- plot(NMDS, type = "t")
 MDSplot
 dev.off()
 
-saveRDS(NMDS, "NMDS_1000_rand_fia_pls_.obj.rds")
+saveRDS(NMDS, "NMDS_1000_rand_fia_pls_2.obj.rds")
 
 variableScores <- NMDS$species
 sampleScores <- NMDS$points
-saveRDS(variableScores, "NMDS_var_scores_rand_fia_pls.rds")
-saveRDS(sampleScores, "NMDS_sample_scores_rand_fia_pls.rds")
+saveRDS(variableScores, "NMDS_var_scores_rand_fia_pls2.rds")
+saveRDS(sampleScores, "NMDS_sample_scores_rand_fia_pls2.rds")
 
 randcell[,38:39] <- sampleScores
 
-png("data/NMDS1_histogram_fia_pls.png")
+png("data/NMDS1_histogram_fia_pls2.png")
 ggplot(randcell, aes(V38, fill = period))+geom_histogram(position = "identity", alpha = 0.5)+xlab("NMDS 1")#+facet_wrap(~period)
 
 dev.off()
 
-png("data/NMDS2_histogram_fia_pls.png")
+png("data/NMDS2_histogram_fia_pls2.png")
 ggplot(randcell, aes(V39, fill = period))+geom_histogram(position = "identity", alpha = 0.5)+xlab("NMDS 1")#+facet_wrap(~period)
 
 dev.off()
 
-saveRDS(randcell, file = "data/NMDS_1000_fia_pls_randcell.rds")
+saveRDS(randcell, file = "data/NMDS_1000_fia_pls_randcell2.rds")
 #MDS.new<- readRDS("outputs/NMDS_bimodal_only_test.rds")
 
 #plot(MDS.new, type = "t")

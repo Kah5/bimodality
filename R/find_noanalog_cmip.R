59ca2e5fb98e85590112f2bb2b87be0aa3b701c3
Kah5 <kellyannheilman@gmail.com>
2017-12-20 17:00:58 -0500

    projections across full climate space--> tip of michigan still missing

--- R/find_noanalog_cmip.R
+++ R/find_noanalog_cmip.R
@@ -1,50 +1,51 @@
-find.noanalog<- function(dens.pr,rcp){
+find.noanalog<- function(clim,rcp){
   
-  dens.pr[,c(paste0("rcp",rcp,"NA"))] <- "Within Range"
-  dens.pr[,c(paste0('rcp',rcp,"NAclim"))] <- "Within Range" # column labeling the reason climate is out of range
-  #dens.pr[,c(paste0('rcp',rcp,"NAclimhigh"))] <- "Within Range" # column labeling the reason climate is out of range
+  clim[,c(paste0("rcp",rcp,"NA"))] <- "Within Range"
+  clim[,c(paste0('rcp',rcp,"NAclim"))] <- "Within Range" # column labeling the reason climate is out of range
+  #clim[,c(paste0('rcp',rcp,"NAclimhigh"))] <- "Within Range" # column labeling the reason climate is out of range
   
   # ranges for historic climate
-  prange <- range(data.frame(dens.pr$MAP1910, dens.pr$MAP2011))
-  trange <- range(dens.pr$pasttmean, dens.pr$modtmean)
+  prange <- range(data.frame(clim$MAP1910, clim$MAP2011), na.rm=TRUE)
+  trange <- range(clim$pasttmean, clim$modtmean, na.rm=TRUE)
   
-  pcvrange <- range(dens.pr$pastdeltaP, dens.pr$moderndeltaP)
-  tcvrange <- range(dens.pr$deltaT, dens.pr$moddeltaT)
+  pcvrange <- range(clim$pastdeltaP, clim$moderndeltaP, na.rm=TRUE)
+  tcvrange <- range(clim$deltaT, clim$moddeltaT, na.rm=TRUE)
   
   # identify climate space outside of the modern/pls range
-  precip.out <- dens.pr[dens.pr[,c(paste0("pr.",rcp))] < prange[1] | dens.pr[,c(paste0("pr.",rcp))] > prange[2], ]
-  pcv.out <- dens.pr[dens.pr[,c(paste0("pr.",rcp,"SI"))] < pcvrange[1] | dens.pr[,c(paste0("pr.",rcp,"SI"))]> pcvrange[2], ]
-  temp.out <- dens.pr[dens.pr[,c(paste0("tn.",rcp))] < trange[1] | dens.pr[,c(paste0("tn.",rcp))]> trange[2], ]
-  tcv.out <- dens.pr[dens.pr[,c(paste0("tn.",rcp,"cv"))] < tcvrange[1] | dens.pr[,c(paste0("tn.",rcp,"cv"))]> tcvrange[2], ]
+  precip.out <- clim[clim[,c(paste0("pr.",rcp))] < prange[1] | clim[,c(paste0("pr.",rcp))] > prange[2], ]
+  pcv.out <- clim[clim[,c(paste0("pr.",rcp,"SI"))] < pcvrange[1] | clim[,c(paste0("pr.",rcp,"SI"))]> pcvrange[2], ]
+  temp.out <- clim[clim[,c(paste0("tn.",rcp))] < trange[1] | clim[,c(paste0("tn.",rcp))]> trange[2], ]
+  tcv.out <- clim[clim[,c(paste0("tn.",rcp,"cv"))] < tcvrange[1] | clim[,c(paste0("tn.",rcp,"cv"))]> tcvrange[2], ]
   
   # identify grid cells with climate space lower than modern/pls
-  pcv.low <- dens.pr[dens.pr[,c(paste0("pr.",rcp,"SI"))] < pcvrange[1],  ]
-  tcv.low <- dens.pr[dens.pr[,c(paste0("tn.",rcp,"cv"))] < tcvrange[1] , ]
-  precip.low <- dens.pr[dens.pr[,c(paste0("pr.",rcp))] < prange[1], ]
-  temp.low <- dens.pr[dens.pr[,c(paste0("tn.",rcp))] < trange[1] , ]
+  pcv.low <- clim[clim[,c(paste0("pr.",rcp,"SI"))] < pcvrange[1],  ]
+  tcv.low <- clim[clim[,c(paste0("tn.",rcp,"cv"))] < tcvrange[1] , ]
+  precip.low <- clim[clim[,c(paste0("pr.",rcp))] < prange[1], ]
+  temp.low <- clim[clim[,c(paste0("tn.",rcp))] < trange[1] , ]
   
-  dens.pr[dens.pr$cell %in% precip.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low precip"
-  dens.pr[dens.pr$cell %in% temp.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low temp"
-  dens.pr[dens.pr$cell %in% tcv.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low temp CV"
-  dens.pr[dens.pr$cell %in% pcv.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low precip SI"
+  clim[clim$cell %in% precip.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low precip"
+  clim[clim$cell %in% temp.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low temp"
+  clim[clim$cell %in% tcv.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low temp CV"
+  clim[clim$cell %in% pcv.low$cell, c(paste0("rcp",rcp,"NAclim"))] <- "Low precip SI"
   
   # identify the gird cells that are higher than modern/pls
-  pcv.high <- dens.pr[ dens.pr[,c(paste0("pr.",rcp,"SI"))]> pcvrange[2], ]
-  tcv.high <- dens.pr[ dens.pr[,c(paste0("tn.",rcp,"cv"))]> tcvrange[2], ]
-  precip.high <- dens.pr[ dens.pr[,c(paste0("pr.",rcp))] > prange[2], ]
-  temp.high <- dens.pr[ dens.pr[,c(paste0("tn.",rcp))]> trange[2], ]
+  pcv.high <- clim[ clim[,c(paste0("pr.",rcp,"SI"))]> pcvrange[2], ]
+  tcv.high <- clim[ clim[,c(paste0("tn.",rcp,"cv"))]> tcvrange[2], ]
+  precip.high <- clim[ clim[,c(paste0("pr.",rcp))] > prange[2], ]
+  temp.high <- clim[ clim[,c(paste0("tn.",rcp))]> trange[2], ]
   
-  dens.pr[dens.pr$cell %in% precip.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High precip"
-  dens.pr[dens.pr$cell %in% temp.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High temp"
-  dens.pr[dens.pr$cell %in% tcv.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High temp CV"
-  dens.pr[dens.pr$cell %in% pcv.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High precip SI"
+  clim[clim$cell %in% precip.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High precip"
+  clim[clim$cell %in% temp.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High temp"
+  clim[clim$cell %in% tcv.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High temp CV"
+  clim[clim$cell %in% pcv.high$cell, c(paste0("rcp",rcp,"NAclim"))] <- "High precip SI"
+  #clim[is.na(clim$cell), c(paste0("rcp",rcp,"NAclim"))] <- "NOdata"
+  
+  clim[clim$cell %in% na.omit(precip.out$cell), c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
+  clim[clim$cell %in% na.omit(temp.out$cell), c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
+  clim[clim$cell %in% na.omit(tcv.out$cell), c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
+  clim[clim$cell %in% na.omit(pcv.out$cell), c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
   
   
-  dens.pr[dens.pr$cell %in% precip.out$cell, c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
-  dens.pr[dens.pr$cell %in% temp.out$cell, c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
-  dens.pr[dens.pr$cell %in% tcv.out$cell, c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
-  dens.pr[dens.pr$cell %in% pcv.out$cell, c(paste0("rcp",rcp,"NA"))] <- "out-of-sample"
-  
-  dens.pr[,c("x","y", "cell", paste0("rcp",rcp,"NA"), paste0('rcp', rcp,"NAclim"))]
+  clim[,c("x","y", "cell", paste0("rcp",rcp,"NA"), paste0('rcp', rcp,"NAclim"))]
   
 }
